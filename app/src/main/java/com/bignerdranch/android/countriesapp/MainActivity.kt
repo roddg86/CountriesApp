d6c38bcd30e672ee07469365707e2197a5824887
roddg86 <roddg@mail.ru>
2022-09-02 18:04:25 +0900

    visible view

--- app/src/main/java/com/bignerdranch/android/countriesapp/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/countriesapp/MainActivity.kt
@@ -2,9 +2,11 @@ package com.bignerdranch.android.countriesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.lifecycleScope
 import com.bignerdranch.android.countriesapp.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -20,18 +22,29 @@ class MainActivity : AppCompatActivity() {
             /* создание корутины(легковесного потока),
             весь код внутри будет выполняться в отдельном потоке*/
             lifecycleScope.launch {
-                /* сделать запрос к api */
-                val countries = restCountriesApi.getCountryByName(countryName)
-                val country = countries[0]
+                try {
+                    /* сделать запрос к api */
+                    val countries = restCountriesApi.getCountryByName(countryName)
+                    val country = countries[0]
 
-                /* размещение сконвертированных данных на view */
-                binding.countryNameTextView.text = country.name
-                binding.capitalTextView.text = country.capital
-                binding.populationTextView.text = formatNumber(country.population)
-                binding.areaTextView.text = formatNumber(country.area)
-                binding.languagesTextView.text = country.languages.joinToString { it.name }
+                    /* размещение сконвертированных данных на view */
+                    binding.countryNameTextView.text = country.name
+                    binding.capitalTextView.text = country.capital
+                    binding.populationTextView.text = formatNumber(country.population)
+                    binding.areaTextView.text = formatNumber(country.area)
+                    binding.languagesTextView.text = country.languages.joinToString { it.name }
 
-                loadSvg(binding.imageView, country.flag)
+                    loadSvg(binding.imageView, country.flag)
+
+                    /* Отображение view после получения информации */
+                    binding.resultLayout.visibility = View.VISIBLE
+                    binding.statusLayout.visibility = View.INVISIBLE
+                }catch (e: Exception){
+                    binding.statusTextView.setText(R.string.status_text_view)
+                    binding.statusImageView.setImageResource(R.drawable.ic_baseline_error_outline_24)
+                    binding.resultLayout.visibility = View.INVISIBLE
+                    binding.statusLayout.visibility = View.VISIBLE
+                }
             }
         }
     }
